9115187ce8b9bb15dbd07d3c2303803eed69a7b9
huangyan <1273383167@qq.com>
2021-12-14 14:16:45 +0800

    Initial commit

---
+++ src/main/java/io/fasttaint/benchmark/FastTaintBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package io.fasttaint.benchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTaintBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastTaintBenchmarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/fasttaint/benchmark/FastTaintBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fasttaint.benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTaintBenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
